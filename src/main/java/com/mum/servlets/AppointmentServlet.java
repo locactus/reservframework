46e9794b8c43fba13db551d5ec46373dfa44827f
靳玉亮 <leo@air.local>
2018-05-15 12:26:43 -0500

    add servlet

--- src/main/java/com/mum/servlets/AppointmentServlet.java
+++ src/main/java/com/mum/servlets/AppointmentServlet.java
@@ -13,6 +13,25 @@ public class AppointmentServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     String action = req.getParameter("action");
+    if(action.equals("add")) {
+      addAppointment(req, resp);
+    } else if(action.equals("list")) {
+      listAllAppointment(req, resp);
+    } else if(action.equals("confirm")) {
+      confirmAppointment(req, resp);
+    }
+  }
+
+  private void confirmAppointment(HttpServletRequest req, HttpServletResponse resp) {
+//    req.getParameter("")
+  }
+
+  private void listAllAppointment(HttpServletRequest req, HttpServletResponse resp) {
+
+  }
+
+  private void addAppointment(HttpServletRequest req, HttpServletResponse resp) {
+
   }
 
   @Override
