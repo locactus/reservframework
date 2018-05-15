42b9385ab249e104084f771ca27f794316711a53
靳玉亮 <leo@air.local>
2018-05-15 11:49:29 -0500

    add json

---
+++ src/main/java/com/mum/servlets/AppointmentServlet.java
@@ -0,0 +1,22 @@
+package com.mum.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/appointment")
+public class AppointmentServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String action = req.getParameter("action");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    doGet(req, resp);
+  }
+}
