a3d6414e12e0b8a2ab84fbbb7c45e7201774c821
靳玉亮 <leo@air.local>
2018-05-15 15:56:53 -0500

    user redirection

--- src/main/java/com/mum/dao/StaffDao.java
+++ src/main/java/com/mum/dao/StaffDao.java
@@ -41,4 +41,37 @@ public class StaffDao {
         }
         return staff;
     }
+
+    public Staff getStaffByUserName(String userName) {
+        Connection conn = DataSource.getInstance().getConnection();
+        Staff staff = null;
+        try {
+            String sql = "SELECT * FROM staff WHERE userName = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, userName);
+            ResultSet rset = pstmt.executeQuery();
+            int numcols = rset.getMetaData().getColumnCount();
+
+            if (!rset.isBeforeFirst()) {
+                // Empty table
+                return null;
+            }
+
+            rset.next();
+            String firstName = rset.getString("firstname");
+            String lastName = rset.getString("lastname");
+            String phoneNumber = rset.getString("phonenumber");
+            String email = rset.getString("email");
+
+            staff = new Staff();
+            staff.setUserName(userName);
+            staff.setFirstName(firstName);
+            staff.setLastName(lastName);
+            staff.setPhoneNumber(phoneNumber);
+            staff.setEmail(email);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return staff;
+    }
 }
--- src/main/java/com/mum/servlets/UserServlet.java
+++ src/main/java/com/mum/servlets/UserServlet.java
@@ -1,5 +1,10 @@
 package com.mum.servlets;
 
+import com.mum.dao.ClientDao;
+import com.mum.dao.StaffDao;
+import com.mum.model.Client;
+import com.mum.model.Staff;
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -10,14 +15,23 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/user")
 public class UserServlet extends HttpServlet {
+
+  private StaffDao staffDao = new StaffDao();
+  private ClientDao clientDao = new ClientDao();
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     String action = req.getParameter("action");
     if("login".equals(action)) {
       //user page is redirected, staff page is redirected to reserveList.jsp
       String username = req.getParameter("username");
-
-      resp.sendRedirect(req.getContextPath() + "/createAppo.jsp");
+      Staff staffByUserName = staffDao.getStaffByUserName(username);
+      Client clientByFirstname = clientDao.getClientByFirstname(username);
+      if(staffByUserName != null) {
+        resp.sendRedirect(req.getContextPath() + "/reserveList.jsp");
+      } else if(clientByFirstname != null){
+        resp.sendRedirect(req.getContextPath() + "/appoList.jsp");
+      }
     }
   }
 
