d174dfe0e6bc632aef9f18ee858c9faf7eae717e
Albert <pospospos2007@gmail.com>
2018-05-16 15:15:29 -0500

    ClientDao 增加getAll

--- src/main/java/com/mum/dao/h2/ClientDao.java
+++ src/main/java/com/mum/dao/h2/ClientDao.java
@@ -3,8 +3,14 @@ package com.mum.dao.h2;
 import com.mum.dao.IClientDao;
 import com.mum.model.Client;
 import java.sql.SQLException;
+import java.util.List;
 
 public class ClientDao extends BaseDao implements IClientDao {
+
+    @Override
+    public List<Client> getAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     @Override
     public Client getClientByClientId(int clientId) throws SQLException {
