89965a7919b4712fd9c5357b8748389e3c3a3881
backslash112 <yangcun@live.com>
2018-05-16 15:15:51 -0500

    fix conflicts

--- src/main/java/com/mum/dao/IClientDao.java
+++ src/main/java/com/mum/dao/IClientDao.java
@@ -2,8 +2,10 @@ package com.mum.dao;
 
 import com.mum.model.Client;
 import java.sql.SQLException;
+import java.util.List;
 
 public interface IClientDAO {
     Client getClientByClientId(int clientId) throws SQLException ;
     Client getClientByFirstname(String firstname) throws SQLException;
+    public List<Client> getAll() throws SQLException;
 }
